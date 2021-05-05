f02fda030d35d6853fd6369fc6c785657eb70661
djkresta <david@omnivector.solutions>
2021-05-05 16:03:42 +0000

    Poetry and tox fixed with hostname example

--- src/fastapi_dev/main.py
+++ src/fastapi_dev/main.py
@@ -1,8 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import subprocess
+import httpx
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Tomato"}
+class Hostname(BaseModel):
+	hostname: str
+
+class UrlResponse(BaseModel):
+	url_info: str
+
+
+def get_hostname():
+    s2_out  = subprocess.check_output(["hostname"])
+    return s2_out
+
+
+@app.post("/hn/", response_model=Hostname)
+async def hostname():
+    hostname = get_hostname()
+    return {"hostname": hostname} 
+
+
+@app.get("/ping")
+async def pong():
+    return {"ping": "pong!"}
+
+
+@app.post("/")
+async def get_url_response(url : UrlResponse):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url.url_info)
+    if response.status_code == 200:
+        return {"status": "ok"}
+    else:
+        return {"status": "bad"} 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,30 @@
-#!/usr/bin/env python3
-
 import pytest
 from httpx import AsyncClient
 
-from fastapi_dev.main import app
+from fastapi_dev.main import app, get_hostname
+URL = "http://httpbin.org/uuid"
 
 
 @pytest.mark.asyncio
-async def test_root():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Tomato"}
+async def test_hello_post():
+	async with AsyncClient(app=app, base_url="http://test") as ac:
+		response = await ac.post("/hn")
+		response_data = response.json()
+		assert response.status_code == 200
+		assert response_data.get("hostname") == 'raton00\n'
+
+
+@pytest.mark.asyncio
+async def test_ping():
+	async with AsyncClient(app=app, base_url="http://test") as ac:
+	    response = await ac.get("/ping")
+	    assert response.status_code == 200
+	    assert response.json() == {"ping": "pong!"}
+
+
+@pytest.mark.asyncio
+async def test_get_url_response():
+	async with AsyncClient(app=app, base_url="http://test") as ac:
+            response = await ac.post("/", json={"url_info": URL})
+
+	assert response.status_code == 200
